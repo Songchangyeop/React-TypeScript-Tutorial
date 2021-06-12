35c29eec2992a76f241765a179c5f01897310263
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-06-13 05:08:39 +0900

    useState, Form 관리 in TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Greetings from './Greetings';
-
+import Counter from './Counter';
+import MyForm from './MyForm';
 function App() {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="Hello" onClick={onClick} />; // 컴포넌트의 렌더링에 필요한 props를 빠뜨리면 에러
-  // Ctrl + Space 눌러보면 props 어떤 값이 들어가야하는지 보여줌
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form.name);
+    console.log(form.description);
+  };
+
+  return (
+    <>
+      <Greetings name="Hello" onClick={onClick} />
+      {/* 컴포넌트의 렌더링에 필요한 props를 빠뜨리면 에러  */}
+      {/* Ctrl + Space 눌러보면 props 어떤 값이 들어가야하는지 보여줌 */}
+      <Counter />
+      <MyForm onSubmit={onSubmit} />
+    </>
+  );
 }
 
 export default App;
